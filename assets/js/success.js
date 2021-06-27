421bb104a2b3728709e250f1b9d5b22f58b67452
stashedup <nathan.d.armstrong@gmail.com>
2021-06-26 23:42:02 -0500

    price

--- assets/js/success.js
+++ assets/js/success.js
@@ -2,7 +2,7 @@ var urlParams = new URLSearchParams(window.location.search);
 var sessionId = urlParams.get('session_id');
 var email = urlParams.get('email');
 var backEndUrl = 'https://stripe.downloadpdf.org';
-var price = '<PRICE>.00'
+var price = '<PRICE>'
 
 var date = new Date();
 var UNIX_timestamp = date.getTime();
